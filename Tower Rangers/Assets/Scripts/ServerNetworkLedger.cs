487f1200084e125218c800383a7b5405cc5c78b1
Bernard Yim <bernard_yim@mymail.sutd.edu.sg>
2017-03-26 23:28:07 +0800

    rough lan lobby working, hardcoded player camera

---
+++ Tower Rangers/Assets/Scripts/ServerNetworkLedger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerNetworkLedger : MonoBehaviour {
+
+	//Health of player
+	//data of land
+	//Spawning information
+	int [] HealthArray = new int[4];
+
+	private int CurrentNumberOfPlayers = 1;
+	private int NUMBEROFPLAYERS = 4;
+
+
+	void OnPlayerConnected(NetworkPlayer player) {
+		Debug.Log("Player " + CurrentNumberOfPlayers++ + " connected from " + player.ipAddress + ":" + player.port);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+
+		//wait until 3 other players connect
+		while (CurrentNumberOfPlayers < NUMBEROFPLAYERS){
+		}
+
+	}
+
+	void onClientConnect(){
+		//assign ID to other player
+		//Allocate health for other player
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Tower Rangers/Assets/Scripts/ViewScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ViewScript : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (!isLocalPlayer)
+			GetComponentInChildren<Camera> ().gameObject.SetActive (false);
+		else {
+			transform.position = new Vector3 (85.91463f, 156.7806f, 71.16507f);
+			transform.eulerAngles = new Vector3 (43.419f, -143.422f, 0f);
+				}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
