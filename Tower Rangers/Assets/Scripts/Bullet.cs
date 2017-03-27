d50cc47e0e7fef6ee22ea65e9e2ee913fc949537
truesanju <sanjaypushparajan@gmail.com>
2017-03-27 16:58:01 +0800

    Tower prefab initial commit

---
+++ Tower Rangers/Assets/Scripts/Bullet.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float bulletSpeed = 0.2f;
+    private Transform target;
+
+    public void setTarget(Transform _target)
+    {
+        target = _target;
+    }
+
+    void moveToTarget()
+    {
+        Vector3 dir = target.position - transform.position;
+        float bulletDistance = bulletSpeed * Time.deltaTime;
+
+        if (dir.magnitude <1)
+        {
+            Debug.LogError("works");
+            Hit();
+            return;
+        }
+
+        transform.Translate(dir.normalized*bulletSpeed,Space.World);
+
+    }
+
+    void Hit()
+    {
+        Destroy(gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (target == null)
+        {
+            Destroy(gameObject);
+        }
+        moveToTarget();
+	
+	}
+}
