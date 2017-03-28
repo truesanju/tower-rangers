b0073ba1cb88261ed92f7076f6d54c2094e6339e
Bernard Yim <bernard_yim@mymail.sutd.edu.sg>
2017-03-29 00:53:41 +0800

    add camera ability to move, change camera angle

--- Tower Rangers/Assets/Scripts/ViewScript.cs
+++ Tower Rangers/Assets/Scripts/ViewScript.cs
@@ -5,18 +5,25 @@ using UnityEngine.Networking;
 
 public class ViewScript : NetworkBehaviour {
 
+	public float ViewSpeed = 1.0f;
 	// Use this for initialization
 	void Start () {
 		if (!isLocalPlayer)
 			GetComponentInChildren<Camera> ().gameObject.SetActive (false);
 		else {
-			transform.position = new Vector3 (85.91463f, 156.7806f, 71.16507f);
-			transform.eulerAngles = new Vector3 (43.419f, -143.422f, 0f);
+			transform.position = new Vector3 (-38.19f, 91.151f, -104.5f);
+			transform.eulerAngles = new Vector3 (40.84f, 43.831f, 0f);
 				}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//transform based off how much we move
+		if (!isLocalPlayer) return;
+
+		var camMoveAmount = Input.GetAxis("Vertical") * Time.deltaTime * ViewSpeed;
+
+		transform.Translate(camMoveAmount, 0, camMoveAmount, Space.World);
+
 	}
 }
