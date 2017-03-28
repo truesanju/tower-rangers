d8956c9aedc92893b4c57aed61f549c4649a5d91
leeyn7 <yuening_lee@mymail.sutd.edu.sg>
2017-03-29 06:32:36 +0800

    Add files via upload

---
+++ Tower Rangers/Assets/Scripts/Buy_tower.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buy_tower : MonoBehaviour {
+
+    public TowerBlueprint towerblueprint;
+
+    LandManager landmanager;
+
+
+    void Start()
+
+    {
+
+        landmanager = LandManager.instance;
+
+    }
+
+
+
+    public void SelectTower()
+
+    {
+
+        Debug.Log("Standard Turret Selected");
+
+        landmanager.SelectTowerToBuild(towerblueprint);
+
+    }
+}
---
+++ Tower Rangers/Assets/Scripts/Land_manager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LandManager : MonoBehaviour {
+
+    public static LandManager instance;
+
+    void Awake()
+
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one Land Manager in scene!");
+            return;
+        }
+        instance = this;
+    }
+
+
+    public GameObject buildEffect;
+
+    //public GameObject sellEffect;
+
+    private TowerBlueprint towerToBuild;
+
+    private Land selectedland;
+
+
+
+    public LandUI landUI;
+
+
+
+    public bool CanBuild { get { return towerToBuild != null; } }
+
+    public bool HasGold { get { return PlayerStats.Gold >= towerToBuild.cost; } }
+
+
+
+    public void selectland(Land land)
+
+    {
+
+        if (selectedland == land)
+
+        {
+
+            DeselectNode();
+
+            return;
+
+        }
+
+
+        selectedland = land;
+
+        towerToBuild = null;
+
+        LandUI.SetTarget(land);
+
+    }
+
+
+
+    public void DeselectNode()
+
+    {
+
+        selectedland = null;
+
+        LandUI.Hide();
+
+    }
+
+
+
+    public void SelectTowerToBuild(TowerBlueprint tower)
+
+    {
+        towerToBuild = tower;
+        DeselectNode();
+    }
+
+
+
+    public TowerBlueprint GetTowerToBuild()
+
+    {
+        return towerToBuild;
+    }
+
+
+
+}
