f37d7a0b3c65aa2760ca90ad3a4ae8418e90068c
Bernard Yim <bernard_yim@mymail.sutd.edu.sg>
2017-03-30 15:39:56 +0800

    modify player's camera to be based off sanjay's scrollable camera

--- Tower Rangers/Assets/Scripts/ViewScript.cs
+++ Tower Rangers/Assets/Scripts/ViewScript.cs
@@ -5,15 +5,25 @@ using UnityEngine.Networking;
 
 public class ViewScript : NetworkBehaviour {
 
-	public float ViewSpeed = 1.0f;
+
+	private Vector3 initialCameraPt;
+	public float sensitivity = 2.0f;
+	private Vector3 dragStartPt;
+	private Vector3 position;
+
+	//public float ViewSpeed = 1.0f;
 	// Use this for initialization
 	void Start () {
 		if (!isLocalPlayer)
 			GetComponentInChildren<Camera> ().gameObject.SetActive (false);
 		else {
-			transform.position = new Vector3 (-38.19f, 91.151f, -104.5f);
-			transform.eulerAngles = new Vector3 (40.84f, 43.831f, 0f);
+			transform.position = new Vector3 (-34.46f, 47.63f, -72.01f);
+			//transform.eulerAngles = new Vector3 (40.84f, 43.831f, 0f);
+			transform.eulerAngles = new Vector3 (0f,46.92f, 0f);
+			GetComponentInChildren<Camera> ().gameObject.transform.eulerAngles = new Vector3 (56.13f, 46.92f, 0f);
+			initialCameraPt = transform.position;
 				}
+
 	}
 	
 	// Update is called once per frame
@@ -21,9 +31,35 @@ public class ViewScript : NetworkBehaviour {
 		//transform based off how much we move
 		if (!isLocalPlayer) return;
 
-		var camMoveAmount = Input.GetAxis("Vertical") * Time.deltaTime * ViewSpeed;
+		//var camMoveAmount = Input.GetAxis("Vertical") * Time.deltaTime * ViewSpeed;
 
-		transform.Translate(camMoveAmount, 0, camMoveAmount, Space.World);
+		//transform.Translate(camMoveAmount, 0, camMoveAmount, Space.World);
 
+		if (Input.GetMouseButtonDown(0))
+		{
+			dragStartPt = Input.mousePosition;
+			return;
+		}
+
+		if (Input.GetMouseButtonDown(1))
+		{
+			transform.position = initialCameraPt;
+			return;
+		}
+
+		if (!Input.GetMouseButton(0)) { return; }
+
+		//Sanjay's singleplayer camera
+		//position = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragStartPt);
+		//Vector3 motion = new Vector3(position.x * sensitivity, position.y * sensitivity, 0);
+		//transform.Translate(motion,Space.Self);
+
+		//Networked Version
+		position = GetComponentInChildren<Camera> ().ScreenToViewportPoint(Input.mousePosition - dragStartPt);
+
+		//With X movement
+		//Vector3 motion = new Vector3(position.x * sensitivity,0, position.y * sensitivity);
+		Vector3 motion = new Vector3(0,0, position.y * sensitivity);
+		transform.Translate(motion,Space.Self);
 	}
 }
