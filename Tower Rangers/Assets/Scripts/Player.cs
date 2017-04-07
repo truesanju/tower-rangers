c491bc872b4395966bfa7b1b945e8c049a9559a3
Bernard Yim <bernard_yim@mymail.sutd.edu.sg>
2017-04-07 09:58:43 +0800

    multiplayer update for multiple spawn points

--- Tower Rangers/Assets/Scripts/Player.cs
+++ Tower Rangers/Assets/Scripts/Player.cs
@@ -11,42 +11,80 @@ public class Player : NetworkBehaviour {
 	private Vector3 position;
 
 	//Game ID variables
-	[SyncVar]
-	int PlayerId = 0;
-	public static HashSet<Player> ActivePlayers = new HashSet<Player>();
+	int PlayerId = 1;
+	int CurrentPlayerId = 1;
 
 	//Networking local player objects
 	public Camera gameCamera;
 	public AudioListener gameAudioListener;
 
 
+	[Command]
+	void CmdSetSpawnClient(){
+		
+		//increase the PlayerID
+		CurrentPlayerId ++;
+
+		RpcSetClientSpawn (CurrentPlayerId);		//Set the player's transform location based off the current player id
+		Debug.Log ("Spawning Player" + CurrentPlayerId + "at Spawn" + CurrentPlayerId);
+
+		//TODO Store the client into the hashset
+
+		//TODO Assign the land to this player
+	}
+
+	[ClientRpc]
+	void RpcSetClientSpawn(int gameId){
+		if (PlayerId != 1) return;
+		PlayerId = gameId;
+
+		//set spawn
+		GameObject SpawnPoint = GameObject.FindGameObjectWithTag ("Spawn"+gameId);
+		//Do transform here
+		transform.position = SpawnPoint.transform.position;
+		transform.eulerAngles = SpawnPoint.transform.eulerAngles;
+		//initialCameraPt = transform.position;
+	}
+
+	public void SetStartSpawn(){
+		//if isServer, spawn at position 1
+		//else do a server command to ask for a spawn position
+		//	Server will assign spawn position based off player id
+
+		if (isServer) {
+			GameObject SpawnPoint = GameObject.FindGameObjectWithTag ("Spawn1");
+			//Do transform here
+			transform.position = SpawnPoint.transform.position;
+			transform.eulerAngles = SpawnPoint.transform.eulerAngles;
+		} else {
+			CmdSetSpawnClient ();
+		}
+	}
+
+
 	public override void OnStartLocalPlayer(){
+		//All players run this once, and on their own device
+
 		//turn on our camera and audiolistener for the local player
 		gameCamera.enabled = true;
 		gameAudioListener.enabled = true;
 		gameObject.name = "LOCAL player";
 
-		//TODO get local player id
 
+		// set spawn dynamically, and assign player id from server
+		SetStartSpawn();
 
-		//Debug.Log ("My game id is " + PlayerId);
-		//TODO, spawn the camera (and alloc map) based off player id 
+		Debug.Log ("My player id is " + PlayerId);
 
 		//set our player controller and camera
-		transform.position = new Vector3 (-34.46f, 47.63f, -72.01f);
-		transform.eulerAngles = new Vector3 (0f,46.92f, 0f);
-		gameCamera.transform.eulerAngles = new Vector3 (56.13f, 46.92f, 0f);
+		//transform.position = new Vector3 (-34.46f, 47.63f, -72.01f);
+		//transform.eulerAngles = new Vector3 (0f,46.92f, 0f);
+		//gameCamera.transform.eulerAngles = new Vector3 (56.13f, 46.92f, 0f);
 		initialCameraPt = transform.position;
 
 		base.OnStartLocalPlayer ();
 	}
 
-	void Start(){
-		ActivePlayers.Add (this);
-		if (isServer) {
-			PlayerId = ActivePlayers.Count;
-		}
-	}
 
 	/*
 	[Command]
